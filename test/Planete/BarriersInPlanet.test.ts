36527c6962b8f95188e813fb2d71381df355b5bd
LLayton <lyes.souifi@gmail.com>
2023-06-20 12:28:03 +0200

    verif if not always true

--- test/Planete/BarriersInPlanet.test.ts
+++ test/Planete/BarriersInPlanet.test.ts
@@ -18,4 +18,16 @@ describe("Est-ce qu'un obtacle est présent en X Y ? ", () => {
   });
 
 });
-
+describe("Est-ce qu'un obtacle n'est pas présent en X Y ? ", () => {
+    const mars = new Planet(10, 10);
+    const Coordinates1 =new Coordinates(6,5);
+    //const Coordinates2 =new Coordinates(7,5);
+  
+    const barrier1 = new barrier(Coordinates1)
+    //const barrier2 = new barrier(Coordinates2);
+    mars.addBarrier(barrier1);
+    it("J'ai créer au préalable une barrière en 6,5 il " , () => {
+      expect(mars.isBarrierXY(5,5)).toBe(false);
+    });
+  
+  });
