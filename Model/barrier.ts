94f9077662e3419ac3eb197fdc6641803299f1fc
LLayton <lyes.souifi@gmail.com>
2023-06-20 12:25:40 +0200

    fix useless parameter in constructor

--- Model/barrier.ts
+++ Model/barrier.ts
@@ -2,11 +2,9 @@ import { Coordinates } from "./Coordinates";
 import { Planet } from "./Planet";
 export class barrier {
   private position: Coordinates;
-  private planet: Planet;
 
-  constructor( position: Coordinates, planet: Planet) {
+  constructor( position: Coordinates) {
     this.position = position;
-    this.planet = planet;
   }
 
   public getPosition(): Coordinates {
